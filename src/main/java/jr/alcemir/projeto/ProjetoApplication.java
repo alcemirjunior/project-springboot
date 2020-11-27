c7b36a9d6b284f88f2315d4ae669d842a56aa6d6
alcemirjunior <alcemirmjunior@gmail.com>
2020-11-27 17:32:49 -0300

    add User entity and Resource

--- src/main/java/jr/alcemir/projeto/ProjetoApplication.java
+++ src/main/java/jr/alcemir/projeto/ProjetoApplication.java
@@ -2,12 +2,14 @@ package jr.alcemir.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"resources"})
 public class ProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
-	}
 
+	}
 }
