69c46f050083b6e807f5f3f167fc540094e3ba21
alcemirjunior <alcemirmjunior@gmail.com>
2020-11-27 22:06:51 -0300

    config h2 and jpa

--- src/main/java/jr/alcemir/projeto/ProjetoApplication.java
+++ src/main/java/jr/alcemir/projeto/ProjetoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"resources"})
+//@ComponentScan(basePackages = {"jr.alcemir.projeto.resources1"})
 public class ProjetoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/jr/alcemir/projeto/resources/UserResource.java
+++ src/main/java/jr/alcemir/projeto/resources/UserResource.java
@@ -1,6 +1,6 @@
-package resources;
+package jr.alcemir.projeto.resources;
 
-import entities.User;
+import jr.alcemir.projeto.entities.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
