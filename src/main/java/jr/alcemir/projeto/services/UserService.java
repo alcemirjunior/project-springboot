ddf96e75036dfbf9555cc0a700477700a2f9a85f
alcemirjunior <alcemirmjunior@gmail.com>
2020-12-08 22:14:34 -0300

    exception handling - findById

--- src/main/java/jr/alcemir/projeto/services/UserService.java
+++ src/main/java/jr/alcemir/projeto/services/UserService.java
@@ -2,6 +2,7 @@ package jr.alcemir.projeto.services;
 
 import jr.alcemir.projeto.entities.User;
 import jr.alcemir.projeto.repositories.UserRepository;
+import jr.alcemir.projeto.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()->new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/jr/alcemir/projeto/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package jr.alcemir.projeto.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+        private static final long serialVersionUID = 1L;
+
+        public ResourceNotFoundException(Object id){
+            super("Resource not found. Id " + id);
+        }
+}
